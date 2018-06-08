c18e1bd8cf003b1398e8ce022736bb0fca543318
Jamil <jamvilleda97@live.com>
2018-06-08 11:48:48 -0600

    Dinosaurios

---
+++ Dinosaurios.h
@@ -0,0 +1,37 @@
+#ifndef DINOSAURIOS_H
+#define DINOSAURIOS_H
+#include <iostream>
+using namespace std;
+
+class Dinosaurios {
+
+  public:
+    string nombre;
+    int altura;
+    int peso;
+    string fechaCreacion;
+    string sexo;
+    int longitud;
+
+  public:
+    //Constructor
+    Dinosaurios();
+    Dinosaurios(string, int, int, string, string, int);
+    //Getters
+    string getNombre();
+    int getAltura();
+    int getPeso();
+    string getFechaCreacion();
+    string getSexo();
+    int getLongitud();
+
+    //Setters
+    void setNombre(string);
+    void setAltura(int);
+    void setPeso(int);
+    void setFechaCreacion(string);
+    void setSexo(string);
+    void setLongitud(int);
+
+};
+#endif
