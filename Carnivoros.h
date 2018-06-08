e3cf2620ec7aeb53682a95517f0f6f8293c7f4e3
LeonardoBanegas99 <leobanegas99@unitec.edu>
2018-06-08 14:11:48 -0600

    carnivoros.h

---
+++ Carnivoros.h
@@ -0,0 +1,29 @@
+#ifndef CARNIVOROS_H
+#define CARNIVOROS_H
+#include <iostream>
+
+#include "Dinosaurios.h"
+
+using namespace std;
+
+class Carnivoros:public Dinosaurios {
+
+  public:
+    string organo;
+    string tipo;
+
+  public:
+    //Constructor
+    Carnivoros();
+    Carnivoros(string,string);
+    //Getters
+    string getOrgano();
+    int getTipo();
+
+    //Setters
+    void setOrgano(string);
+    void setTipo(string);
+    virtual void toString();
+
+};
+#endif
